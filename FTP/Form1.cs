55214577d406230012b9d005eb81a796220a617e
hungbb <thanhhung94@gmail.com>
2014-11-15 17:13:43 +0700

    Thư viện được dùng

--- FTP/Form1.cs
+++ FTP/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace FTP
 {
